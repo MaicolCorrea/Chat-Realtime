e447a335f44d0acfd3fe0bbd86dec246b16ef973
MaicolCorrea <maicolsmithcorrealopez@gmail.com>
2025-02-11 21:01:24 -0500

    Finalización de chat sin autenticación

--- server/index.js
+++ server/index.js
@@ -19,18 +19,52 @@ const io = new Server(server, {
 const db = createClient({
     url: 'libsql://grateful-timeslip-analistaist1.turso.io',
     authToken: process.env.DB_TOKEN
-});
+}); 
 
-io.on('connection', (socket) => {
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        username TEXT
+    )
+`);
+
+io.on('connection', async (socket) => {
     console.log('a user has connected!');
 
     socket.on('disconnect', () => {
         console.log('an user has disconnected');
     });
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        let username = socket.handshake.auth.username ?? 'anonymous'
+        try {
+            result = await db.execute({
+                sql: 'INSERT INTO messages (content, username) VALUES (:msg, :username)',
+                args: { msg, username }
+            });
+        } catch (e) {
+            console.error(e)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     });
+
+    if (!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content, username FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            });
+
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString(), row.username);
+            });
+        } catch (e) {
+            console.error(e)
+        }
+    }
 });
 
 app.use(logger('dev'));
